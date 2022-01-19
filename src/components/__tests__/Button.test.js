3ad19632a42ee194f4bbcbd092b90e931d46eb61
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-18 19:20:57 -0500

    Modify to build and test the Buttons -- base, confirm, danger, clickable, diasabled

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -9,28 +9,30 @@ afterEach(cleanup);
 it("renders without crashing", () => {
   render(<Button />);
 });
-
+// 1
 it.skip("renders its `children` prop as text", () => {
   const { getByText } = render(<Button>Default</Button>);
   expect(getByText("Default")).toBeInTheDocument();
 });
-
+// 2
 it.skip("renders a default button style", () => {
   const { getByText } = render(<Button>Default</Button>);
   expect(getByText("Default")).toHaveClass("button");
 });
-
+// 3
 it.skip("renders a confirm button", () => {
   const { getByText } = render(<Button confirm>Confirm</Button>);
   expect(getByText("Confirm")).toHaveClass("button--confirm");
 });
-
+// 4
 it.skip("renders a danger button", () => {
   const { getByText } = render(<Button danger>Danger</Button>);
   expect(getByText("Danger")).toHaveClass("button--danger");
 });
+// 5
+// it.skip("renders a clickable button", () => {
+it("renders a clickable button", () => {
 
-it.skip("renders a clickable button", () => {
   const handleClick = jest.fn();
   const { getByText } = render(
     <Button onClick={handleClick}>Clickable</Button>
@@ -42,8 +44,9 @@ it.skip("renders a clickable button", () => {
 
   expect(handleClick).toHaveBeenCalledTimes(1);
 });
-
+// 6
 it.skip("renders a disabled button", () => {
+// it("renders a disabled button", () => {
   const handleClick = jest.fn();
   const { getByText } = render(
     <Button disabled onClick={handleClick}>
