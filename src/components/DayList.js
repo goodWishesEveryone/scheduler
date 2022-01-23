73e371999e716ae6f456564d44334bb11bc281cd
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-22 23:20:38 -0500

    Add DayList which serves as a container to hold all the <DayListItem> components

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+// serves as a container to hold all the <DayListItem> components
+export default function DayList(props) {
+  console.log(props.days);
+  const days = props.days.map( day => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day} // the currently selected day
+        setDay={props.setDay}             // sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday"
+      />
+    )
+  });
+  return days;
+};
