d5763b58f5a7b5ef88ca8e2944182983c5c1c091
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-27 06:24:21 -0500

    Import axios & useEffect; remove the days array and use useState to add the days state to the Application component and initialized as an empty array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,39 +1,90 @@
-// import React from "react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
+import { all } from "micromatch";
 
-const days = [
+// mock data for appointments
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
 ];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday"); // default day state to "Monday"
+  const [days, setDays] = useState([]);  //use useState to add a days state to the Application component; initialized as an empty array.
+  
   console.log(day);
-
   const changeDay = (value) => {
     return setDay(value);
+  //const setDay = day => setDay({...state, day})
   };
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get('http://localhost:8001/api/days')),
+      Promise.resolve(axios.get('http://localhost:8001/api/')),
+      Promise.resolve(axios.get('http://localhost:8001/api/interviewers'))
+    ]).then((all) => {
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers:all[2].data}));
+      })
+  }, [])
+  
+  const appointmentList = appointments.map((appointment) => {
+    return (
+
+      // The Scheduler activity
+      // <Appointment key={appointment.id} {...appointment} />
+
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        {/* sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -43,7 +94,6 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           {/* Incorporate the DayList component and Passing day and days to <DayList> */}
           <DayList days={days} value={day} onChange={changeDay} />
-      
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -51,9 +101,10 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
-        <section className="schedule " />
+      <section className="schedule ">
+        {/* Replace this with the schedule elements during the "The Scheduler" activity:  map over the appointments array to create a list */}
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
