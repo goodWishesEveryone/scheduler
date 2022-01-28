6eb0b2cb97d4245a76593652a5b854e673206416
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-28 06:10:19 -0500

    Create a function getInterview(state, interview) in src/helpers/selectors.js.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,7 @@
 // selector, is a function that accepts state as an argument and returns data that is derived from that state.
-// getAppointmentsForDay will return an array of appointments for the given day
+
+//////////////////  ---  getAppointmentsForDay  ---  ///////////////////
+// will return an array of appointments for the given day
 
 export function getAppointmentsForDay(state, day) {
   const filteredAppointments = state.days.filter(date => date.name === day);
@@ -9,8 +11,22 @@ export function getAppointmentsForDay(state, day) {
   if (filteredAppointments.length){
     appointments = filteredAppointments[0].appointments.map(x => state.appointments[x]);
   }
-
   return appointments;
-
 }
 
+
+//////////////////  ---  getInterview  ---  //////////////////
+// will return a new object containing the interview data when we pass it an object that contains the interviewer; Otherwise, returns null.
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null
+  }
+
+  const interviewObj = { 
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  }
+  
+  return interviewObj;
+}
