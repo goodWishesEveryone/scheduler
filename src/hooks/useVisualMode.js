b70354b3291ee2f803df7c72f6bd3d2693d83976
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-28 17:50:35 -0500

    Refractored to ensure all tests passed

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,33 @@
+import { last } from "lodash";
 import { useState } from "react";
 
-export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
-  const [history, setHistory] = useState([initialMode]);
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); // This line is new!
 
-  function transition(newMode, replace = false) {
-    if (replace) {
-      setHistory(prev => [...prev].slice(0, -1))
-    } 
-    setHistory(prev => [...prev, newMode]);
-    setMode(newMode);
+  function transition(mode, replace = false) {
+    if(replace) {
+      let currHistory = history;
+      // const currMode = history[history.length - 1];
+      currHistory[currHistory.length -1] = mode
+      setHistory(currHistory) 
+      setMode(mode); 
+    }
+   else {
+    setHistory([...history, mode])
+    setMode(mode);
+   }
   }
 
   function back() {
+    // remove last element from history array
     if (history.length > 1) {
-      const newHistory = history.slice(0, -1);
-      setHistory(newHistory);
-      setMode(newHistory[newHistory.length -1]);
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else {
+      return;
     }
   }
 
   return { mode, transition, back };
-};
- 
+}
