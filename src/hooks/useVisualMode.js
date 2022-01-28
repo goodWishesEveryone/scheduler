7520853f02850619a275668652cc44d023df3df2
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-28 03:09:12 -0500

    Add hooks/_tests_/useVisualMode.test.js and src/hooks/useVisualMode.js

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev].slice(0, -1))
+    } 
+    setHistory(prev => [...prev, newMode]);
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, -1);
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length -1]);
+    }
+  }
+
+  return { mode, transition, back };
+};
+ 
