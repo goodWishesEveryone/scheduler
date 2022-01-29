16e0055da9d459226c088142eb8cf5b2ef2c98da
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-28 20:06:20 -0500

    Refractored to use custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,7 +4,7 @@ import "./styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
-
+import Form from "./Form";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -13,23 +13,37 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const { id, time, interview , interviewer } = props;
   const { mode, transition, back } = useVisualMode(
+    // When props.interview contains a value, then pass useVisualMode the SHOW mode, if empty, then should pass EMPTY
     props.interview ? SHOW : EMPTY
   );
 
-  
   return (
     <article className="appointment">
       <Header time={props.time} />
+      {/* {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )} */}
+
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
-          student={interview.student}
-          interviewer={interview.interviewer}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
         />
       )}
-      
+      {mode === CREATE && (
+        <Form
+          name={props.name}
+          value={props.value}
+          interviewers={props.interviewers}
+          onCancel={back}
+        />  
+      )}
     </article>
   );
 }
