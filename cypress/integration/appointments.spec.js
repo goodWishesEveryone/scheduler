b3d10ea2cb1cc3664f80f347367a085cd59602f5
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-06 15:14:19 -0500

    Add integration testing using Cypress for appointment features: edit and cancel an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,17 +1,51 @@
-// testing using Cypress for appointments feature
+// End-to-End Testing using Cypress for appointments feature
 
-describe("Appointment test", () => {
+describe("Appointments", () => {
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
+
     cy.visit("/");
+
     cy.contains("Monday");
   });
+
+  ///////// --- book an interview --- ////////
   it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
-    cy.get("input.appointment__create-input").type("Lydia Miller-Jones");
-    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+
     cy.contains("Save").click();
+
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  ///////// --- edit an interview --- ////////
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  ///////// --- cancel an interview --- ////////
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
